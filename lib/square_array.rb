67439893990bb6a9efdffa897634627e9b2da02e
Hannah Drury <hannah_drury@yahoo.com>
2020-01-17 19:23:21 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
  count = 0
  arrayNew = []
   while count < array.length do
-    array[count] = array[count] * array[count]
+    array[count] = (array[count] * array[count])
     count += 1
   end
 end
